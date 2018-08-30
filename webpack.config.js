4692676bb91827158b77ffb52b5c8171da4d4c1b
Xyf <1278399946@qq.com>
2018-08-30 13:42:27 +0800

    商品模块

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,10 @@ var config = {
         'list'              : ['./src/page/list/index.js'],
         'detail'            : ['./src/page/detail/index.js'],
         'cart'              : ['./src/page/cart/index.js'],
-        'order-confirm'     : ['./src/page/order-confirm/index.js'],
-        'order-list'        : ['./src/page/order-list/index.js'],
-        'order-detail'      : ['./src/page/order-detail/index.js'],
-        'payment'           : ['./src/page/payment/index.js'],
+        //'order-confirm'     : ['./src/page/order-confirm/index.js'],
+        //'order-list'        : ['./src/page/order-list/index.js'],
+        //'order-detail'      : ['./src/page/order-detail/index.js'],
+        //'payment'           : ['./src/page/payment/index.js'],
         'user-login'        : ['./src/page/user-login/index.js'],
         'user-register'     : ['./src/page/user-register/index.js'],
         'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
@@ -36,7 +36,7 @@ var config = {
         'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
         'result'            : ['./src/page/result/index.js'],
-        'about'             : ['./src/page/about/index.js'],
+        //'about'             : ['./src/page/about/index.js'],
     },
     output: {
         path        : __dirname + '/dist/',
@@ -82,10 +82,10 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')),
         new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')),
         new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
-        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
-        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
-        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
-        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
+        //new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+        //new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        //new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        //new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
@@ -93,18 +93,18 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
-        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall')),
+        //new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall')),
     ],
-    devServer: {
-        port: 8088,
-        inline: true,
-        proxy : {
-            '**/*.do' : {
-                target: 'http://test.happymmall.com',
-                changeOrigin : true
-            }
-        }
-    }
+    // devServer: {
+    //     port: 8088,
+    //     inline: true,
+    //     proxy : {
+    //         '**/*.do' : {
+    //             target: 'http://test.happymmall.com',
+    //             changeOrigin : true
+    //         }
+    //     }
+    // }
 
 };
 
